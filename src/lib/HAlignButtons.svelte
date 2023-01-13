baeaf3abed3e42039b7df739daf73b07f66dbab9
miyabi-satoh <miyabi.satoh@gmail.com>
2023-01-13 11:56:47 +0900

    新規作成

---
+++ src/lib/HAlignButtons.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import { Tooltip } from 'flowbite-svelte';
+
+	export let canvasWidth: number;
+	export let selfWidth: number = 0;
+	export let value: number;
+</script>
+
+<div class="flex gap-3">
+	<button class="btn-icon" on:click={() => (value = 0)}>
+		<Icon icon="mdi:format-horizontal-align-left" height="auto" />
+	</button>
+	<Tooltip style="light">右寄せ</Tooltip>
+	<button class="btn-icon" on:click={() => (value = Math.floor((canvasWidth - selfWidth) / 2))}>
+		<Icon icon="mdi:format-horizontal-align-center" height="auto" />
+	</button>
+	<Tooltip style="light">中央寄せ</Tooltip>
+	<button class="btn-icon" on:click={() => (value = canvasWidth - selfWidth)}>
+		<Icon icon="mdi:format-horizontal-align-right" height="auto" />
+	</button>
+	<Tooltip style="light">左寄せ</Tooltip>
+</div>
---
+++ src/lib/VAlignButtons.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import { Tooltip } from 'flowbite-svelte';
+
+	export let canvasHeight: number;
+	export let selfHeight: number = 0;
+	export let value: number;
+</script>
+
+<div class="flex gap-3">
+	<button class="btn-icon" on:click={() => (value = 0)}>
+		<Icon icon="mdi:format-vertical-align-top" height="auto" />
+	</button>
+	<Tooltip style="light">上寄せ</Tooltip>
+	<button class="btn-icon" on:click={() => (value = Math.floor((canvasHeight - selfHeight) / 2))}>
+		<Icon icon="mdi:format-vertical-align-center" height="auto" />
+	</button>
+	<Tooltip style="light">中央寄せ</Tooltip>
+	<button class="btn-icon" on:click={() => (value = canvasHeight - selfHeight)}>
+		<Icon icon="mdi:format-vertical-align-bottom" height="auto" />
+	</button>
+	<Tooltip style="light">下寄せ</Tooltip>
+</div>
