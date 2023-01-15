204e8b509f787ccf13dbed19d4382d48c93022d7
miyabi-satoh <miyabi.satoh@gmail.com>
2023-01-16 02:33:06 +0900

    clickイベントを追加

--- src/lib/HAlignButtons.svelte
+++ src/lib/HAlignButtons.svelte
@@ -1,22 +1,44 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
 	import { Tooltip } from 'flowbite-svelte';
+	import { createEventDispatcher } from 'svelte';
 
 	export let canvasWidth: number;
 	export let selfWidth: number = 0;
 	export let value: number;
+
+	const dispatch = createEventDispatcher();
+	type HAlign = 'left' | 'center' | 'right';
+	function onClickDispatch(align: HAlign) {
+		dispatch('click', { align });
+	}
+
+	function handleClickLeft() {
+		value = 0;
+		onClickDispatch('left');
+	}
+
+	function handleClickCenter() {
+		value = Math.floor((canvasWidth - selfWidth) / 2);
+		onClickDispatch('center');
+	}
+
+	function handleClickRight() {
+		value = canvasWidth - selfWidth;
+		onClickDispatch('right');
+	}
 </script>
 
 <div class="flex gap-3">
-	<button class="btn-icon" on:click={() => (value = 0)}>
+	<button class="btn-icon" on:click={handleClickLeft}>
 		<Icon icon="mdi:format-horizontal-align-left" height="auto" />
 	</button>
 	<Tooltip style="light">右寄せ</Tooltip>
-	<button class="btn-icon" on:click={() => (value = Math.floor((canvasWidth - selfWidth) / 2))}>
+	<button class="btn-icon" on:click={handleClickCenter}>
 		<Icon icon="mdi:format-horizontal-align-center" height="auto" />
 	</button>
 	<Tooltip style="light">中央寄せ</Tooltip>
-	<button class="btn-icon" on:click={() => (value = canvasWidth - selfWidth)}>
+	<button class="btn-icon" on:click={handleClickRight}>
 		<Icon icon="mdi:format-horizontal-align-right" height="auto" />
 	</button>
 	<Tooltip style="light">左寄せ</Tooltip>
--- src/lib/VAlignButtons.svelte
+++ src/lib/VAlignButtons.svelte
@@ -1,22 +1,44 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
 	import { Tooltip } from 'flowbite-svelte';
+	import { createEventDispatcher } from 'svelte';
 
 	export let canvasHeight: number;
 	export let selfHeight: number = 0;
 	export let value: number;
+
+	const dispatch = createEventDispatcher();
+	type VAlign = 'top' | 'middle' | 'bottom';
+	function onClickDispatch(align: VAlign) {
+		dispatch('click', { align });
+	}
+
+	function handleClickTop() {
+		value = 0;
+		onClickDispatch('top');
+	}
+
+	function handleClickMiddle() {
+		value = Math.floor((canvasHeight - selfHeight) / 2);
+		onClickDispatch('middle');
+	}
+
+	function handleClickBottom() {
+		value = canvasHeight - selfHeight;
+		onClickDispatch('bottom');
+	}
 </script>
 
 <div class="flex gap-3">
-	<button class="btn-icon" on:click={() => (value = 0)}>
+	<button class="btn-icon" on:click={handleClickTop}>
 		<Icon icon="mdi:format-vertical-align-top" height="auto" />
 	</button>
 	<Tooltip style="light">上寄せ</Tooltip>
-	<button class="btn-icon" on:click={() => (value = Math.floor((canvasHeight - selfHeight) / 2))}>
+	<button class="btn-icon" on:click={handleClickMiddle}>
 		<Icon icon="mdi:format-vertical-align-center" height="auto" />
 	</button>
 	<Tooltip style="light">中央寄せ</Tooltip>
-	<button class="btn-icon" on:click={() => (value = canvasHeight - selfHeight)}>
+	<button class="btn-icon" on:click={handleClickBottom}>
 		<Icon icon="mdi:format-vertical-align-bottom" height="auto" />
 	</button>
 	<Tooltip style="light">下寄せ</Tooltip>
